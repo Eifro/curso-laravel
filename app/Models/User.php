5a027ad1c3bee8ecf4e864aaab00e2e71b363666
Eifro <afrotricks19@gmail.com>
2024-03-18 17:49:50 -0500

    mutadores y accesores

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class User extends Authenticatable
 {
+    /* 
+        Mutadores: Permite controlar y transformar el texto ingresado en un input para guardarlo en la base de datos. Transforman el valor antes de almacenarlos.
+        Accesores: Transforman el valor después de almacenarlos, pero no lo modifica en la base de datos.
+    */
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -42,4 +48,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // mutador para el campo name de la table users - formatea el texto a minúsculas
+    protected function name():Attribute
+    {
+        return new Attribute(
+            get: function($value) {
+                return ucwords($value);
+            },
+            set: function($value) {
+                return strtolower($value);
+            }
+        );
+    }
 }
